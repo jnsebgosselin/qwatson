d1a326c2d455af983acd38328b053dc943bb9a5b
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2018-06-04 11:59:48 -0400

    Updated the copyright notice

--- watson/watson.py
+++ watson/watson.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+# Copyright © 2018 tailordev
+# http://tailordev.github.io/Watson/
+
+# Copyright © 2018 Jean-Sébastien Gosselin
+# https://github.com/jnsebgosselin/qwatson
+#
+# This file is part of QWatson.
+# Licensed under the terms of the GNU General Public License.
+
 import watson
 import arrow
 
