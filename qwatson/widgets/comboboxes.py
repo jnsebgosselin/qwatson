ad359d471fe932f6f0afdf8970bffa8b3986a137
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2018-06-05 11:11:53 -0400

    Refactor and document ComboBoxEdit

--- qwatson/widgets/comboboxes.py
+++ qwatson/widgets/comboboxes.py
@@ -44,42 +44,53 @@ class ComboBoxEdit(QWidget):
         super(ComboBoxEdit, self).setFixedHeight(height)
 
     def eventFilter(self, widget, event):
-        accept_event = (
+        """
+        Check for events to accept the edits and modify the content of the
+        combobox accordingly.
+        """
+        event_is_accepted = (
             event.type() == QEvent.FocusOut or
             (event.type() == QEvent.KeyPress and
              event.key() in [Qt.Key_Enter, Qt.Key_Return])
             )
 
-        if accept_event:
+        if event_is_accepted:
             self.linedit.setVisible(False)
             self.combobox.setVisible(True)
 
             new_name = self.linedit.text()
             self.linedit.clear()
             if new_name:
-                if self.combobox.findText(new_name) == -1:
-                    if self._edit_mode == 'add':
+                new_name_exists = self.combobox.findText(new_name) != -1
+                if self._edit_mode == 'add':
+                    if new_name_exists is False:
                         self.combobox.addItem(new_name)
+                        self.setCurentText(new_name)
                         self.sig_item_added.emit(new_name)
-                    elif self._edit_mode == 'rename':
-                        old_name = self.combobox.currentText()
+                    else:
+                        self.setCurentText(new_name)
+                elif self._edit_mode == 'rename':
+                    old_name = self.combobox.currentText()
+                    if old_name != new_name:
                         current_index = self.combobox.currentIndex()
-                        # Update the gui.
                         self.combobox.removeItem(current_index)
-                        self.combobox.insertItem(current_index, new_name)
-                        # Update the client.
+                        if new_name_exists is False:
+                            self.combobox.insertItem(current_index, new_name)
+                        self.setCurentText(new_name)
                         self.sig_item_renamed.emit(old_name, new_name)
 
-                self.setCurentText(new_name)
         return QWidget.eventFilter(self, widget, event)
 
     def currentText(self):
+        """Return the current text of the combobox."""
         return self.combobox.currentText()
 
     def currentIndex(self):
+        """Return the current index of the combobox."""
         return self.combobox.currentIndex()
 
     def setCurentText(self, name):
+        """Set the combobox to the index matching the provided name."""
         self.combobox.blockSignals(True)
         self.combobox.setCurrentIndex(self.combobox.findText(name))
         self.combobox.blockSignals(False)
@@ -89,9 +100,14 @@ class ComboBoxEdit(QWidget):
         self.combobox.addItems(items)
 
     def removeItem(self, index):
+        """Remove the item at the specified index in the combobox."""
         self.combobox.removeItem(index)
 
     def set_edit_mode(self, mode):
+        """
+        Enter edit mode either to add a new item or to rename an existing
+        item.
+        """
         if mode in ['add', 'rename']:
             self._edit_mode = mode
             if mode == 'rename':
