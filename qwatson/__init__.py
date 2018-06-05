6cc1e6fab6225721c5963678c1d973dec71fa695
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2018-06-05 09:15:18 -0400

    Change dev version in init

--- qwatson/__init__.py
+++ qwatson/__init__.py
@@ -25,7 +25,7 @@ import os
 import sys
 
 
-version_info = (0, 0, 1, 'dev')
+version_info = (0, 1, 0, 'dev')
 __version__ = '.'.join(map(str, version_info))
 __appname__ = 'QWatson'
 __namever__ = __appname__ + " " + __version__
