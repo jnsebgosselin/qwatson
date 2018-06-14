310581cb5a9bbbc23c88688a4f06cdaa701de1c2
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-06-14 13:59:51 -0400

    Create a ColoredFrame class
    A simple frame widget with utility methods to easily set the color of
    its background.

--- qwatson/widgets/layout.py
+++ qwatson/widgets/layout.py
@@ -9,7 +9,7 @@
 
 # ---- Third party imports
 
-from PyQt5.QtWidgets import QFrame
+from PyQt5.QtWidgets import QFrame, QStyleOption
 
 
 class HSep(QFrame):
@@ -24,3 +24,23 @@ class VSep(QFrame):
     def __init__(self, parent=None):
         super(VSep, self).__init__(parent)
         self.setFrameStyle(53)
+
+
+class ColoredFrame(QFrame):
+    """
+    A simple frame widget with utility methods to easily set the color
+    of its background.
+    """
+    def __init__(self, color=None, parent=None):
+        super(ColoredFrame, self).__init__(parent)
+        self.setAutoFillBackground(True)
+
+    def set_background_color(self, colorname):
+        if colorname == 'light':
+            color = QStyleOption().palette.light().color()
+        else:
+            color = QStyleOption().palette.base().color()
+
+        palette = self.palette()
+        palette.setColor(self.backgroundRole(), color)
+        self.setPalette(palette)
