69e92cc942d6dae0c9a9cc9f03ab149d0fe1f453
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-06-14 13:41:47 -0400

    Update init and gitignore

--- qwatson/views/__init__.py
+++ qwatson/views/__init__.py
@@ -6,5 +6,6 @@
 # This file is part of QWatson.
 # Licensed under the terms of the GNU General Public License.
 """
-A compendium of widgets that, ideally, know nothing about Watson.
+A compendium of widgets that are specifically tailored to display data and
+route user commands to Watson.
 """
