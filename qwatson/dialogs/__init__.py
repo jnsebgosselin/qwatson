1137501101883fb8443cd165a40044d1e94f53c5
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-06-20 09:19:20 -0400

    Reworked the text of the module dialogs

--- qwatson/dialogs/__init__.py
+++ qwatson/dialogs/__init__.py
@@ -6,6 +6,6 @@
 # This file is part of QWatson.
 # Licensed under the terms of the GNU General Public License.
 """
-A compendium of widgets that are specifically tailored to display data and
+A compendium of dialogs that are specifically tailored to display data and
 route user commands to Watson.
 """
