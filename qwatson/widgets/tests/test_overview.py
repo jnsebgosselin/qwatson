d44dd7a83c6fe5f9a7775e4134a7657d51d7beed
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-07-16 16:46:30 -0400

    Test with less activities
    Create a framefile that span over the current week with 2 activity of 6
    hours per day, between 6:00 AM and 12:00 PM and 6:00PM and 12:00AM.

--- qwatson/widgets/tests/test_overview.py
+++ qwatson/widgets/tests/test_overview.py
@@ -46,18 +46,19 @@ def overview_creator(qtbot, mocker):
 
 def create_framefile(mocker):
     """
-    Create a framefile that span over the current week with one activity
-    every hour.
+    Create a framefile that span over the current week with 2 activity of
+    6 hours per day, between 6:00 AM and 12:00 PM and 6:00PM and 12:00AM.
     """
     qwatson = QWatson(APPDIR)
 
-    i = 0
+    i = 1
     while True:
         start = SPAN[0].shift(hours=i*6)
         mocker.patch('arrow.now', return_value=start)
         qwatson.client.start('test_overview')
-        i += 1
-        stop = SPAN[0].shift(hours=i*6)
+
+        stop = SPAN[0].shift(hours=(i+1)*6)
+        stop = SPAN[0].shift(hours=(i+1)*6)
         mocker.patch('arrow.now', return_value=stop)
         qwatson.stop_watson(
             message='activity #%s' % i, project='test_overview', tags=None,
@@ -65,6 +66,8 @@ def create_framefile(mocker):
 
         if stop >= SPAN[1]:
             break
+        i += 2
+
     qwatson.client.save()
 
 
@@ -76,7 +79,7 @@ def test_overview_init(overview_creator):
 
     assert overview.isVisible()
 
-    assert overview.table_widg.total_seconds == 7*24*1*60*60
+    assert overview.table_widg.total_seconds == 7*(2*6)*60*60
     assert len(overview.table_widg.tables) == 7
     assert overview.table_widg.last_focused_table is None
     assert overview.table_widg.date_span == SPAN
@@ -100,10 +103,17 @@ def test_overview_row_selection(overview_creator):
     qtbot.mouseClick(
         tables[1].view.viewport(), Qt.LeftButton, pos=visual_rect.center())
 
+    # Assert that all but one table have a row and a frame selected.
     assert overview.table_widg.last_focused_table == tables[1]
-    assert overview.table_widg.last_focused_table.get_selected_row() == 1
+    for table in tables:
+        if table != overview.table_widg.last_focused_table:
+            assert table.get_selected_row() is None
+            assert table.get_selected_frame_index() is None
+        else:
+            assert table.get_selected_row() == 1
+            assert table.get_selected_frame_index() == 2 + 2 - 1
 
-    # Mouse click on the third row of the second table.
+    # Mouse click on the first row of the second table.
 
     index = tables[2].view.proxy_model.index(2, 0)
     visual_rect = tables[2].view.visualRect(index)
@@ -112,13 +122,16 @@ def test_overview_row_selection(overview_creator):
         tables[2].view.viewport(), Qt.LeftButton, pos=visual_rect.center())
 
     assert overview.table_widg.last_focused_table == tables[2]
-    assert tables[2].get_selected_row() == 2
 
-    # Assert that all tables but 2 have no selected row
+    # Assert that all tables but 2 have no selected row and frame.
 
     for table in tables:
         if table != overview.table_widg.last_focused_table:
             assert table.get_selected_row() is None
+            assert table.get_selected_frame_index() is None
+        else:
+            assert table.get_selected_row() == 0
+            assert table.get_selected_frame_index() == 2 + 2 + 1 - 1
 
 
 if __name__ == "__main__":
