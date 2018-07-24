f3ea4a0e05f6c6ff4d32920df95d4bb25f7575a6
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-07-24 16:31:02 -0400

    Improve ProjectManager code and interface

--- qwatson/managers/projects.py
+++ qwatson/managers/projects.py
@@ -13,34 +13,36 @@ import sys
 # ---- Third party imports
 
 from PyQt5.QtCore import pyqtSignal as QSignal
-from PyQt5.QtWidgets import QApplication, QGridLayout, QMessageBox, QWidget
+from PyQt5.QtCore import Qt, QEvent
+from PyQt5.QtWidgets import (QApplication, QGridLayout, QMessageBox, QWidget,
+                             QLineEdit, QComboBox)
 
 # ---- Local imports
 
-from qwatson.widgets.comboboxes import ComboBoxEdit
 from qwatson.widgets.toolbar import ToolBarWidget, QToolButtonSmall
 from qwatson.utils import icons
+from qwatson.models.projectmodel import WatsonProjectModel
 
 
 class ProjectManager(QWidget):
     """
     A toolbar composed of a combobox to show the existing projects and
-    toolbuttons to edit, add, and delete project from the combobox.
+    toolbuttons to send request to the QWatson mainwindow to add, rename, or
+    delete projects from the databse.
     """
-    sig_project_renamed = QSignal(str, str)
-    sig_project_added = QSignal(str)
+    sig_rename_project = QSignal(str, str)
+    sig_add_project = QSignal(str)
+    sig_del_project = QSignal(str)
     sig_project_changed = QSignal(int)
-    sig_project_removed = QSignal(str)
 
-    def __init__(self, projects=[], parent=None):
+    def __init__(self, client, parent=None):
         super(ProjectManager, self).__init__(parent)
-
+        self.client = client
+        self.model = WatsonProjectModel(client)
         self.setup()
-        self.set_projects(projects)
 
     def setup(self):
         """Setup the widget with the provided arguments."""
-
         self.project_cbox = self.setup_combobox()
         self.toolbar = self.setup_toolbar()
 
@@ -57,12 +59,12 @@ class ProjectManager(QWidget):
         Setup the combobox where project can be selected, added, edited, and
         removed.
         """
-        project_cbox = ComboBoxEdit()
+        project_cbox = ProjectView(self.model)
+        project_cbox.setFixedHeight(icons.get_iconsize('small').height())
 
         # Relay signals from the combobox.
-        project_cbox.setFixedHeight(icons.get_iconsize('small').height())
-        project_cbox.sig_item_renamed.connect(self.sig_project_renamed.emit)
-        project_cbox.sig_item_added.connect(self.sig_project_added.emit)
+        project_cbox.sig_rename_project.connect(self.sig_rename_project.emit)
+        project_cbox.sig_add_project.connect(self.sig_add_project.emit)
         project_cbox.currentIndexChanged.connect(self.project_changed)
 
         return project_cbox
@@ -72,19 +74,18 @@ class ProjectManager(QWidget):
         toolbar = ToolBarWidget()
 
         self.btn_add = QToolButtonSmall('plus')
-        self.btn_add.clicked.connect(self.btn_add_isclicked)
+        self.btn_add.clicked.connect(self.project_cbox.add_project)
         self.btn_add.setToolTip("Add a new project")
 
         self.btn_rename = QToolButtonSmall('edit')
-        self.btn_rename.clicked.connect(self.btn_rename_isclicked)
+        self.btn_rename.clicked.connect(self.project_cbox.rename_project)
         self.btn_rename.setToolTip("Rename the current project")
 
         self.btn_remove = QToolButtonSmall('clear')
         self.btn_remove.clicked.connect(self.btn_remove_isclicked)
         self.btn_remove.setToolTip("Delete the current project")
 
-        items = [self.btn_add, self.btn_rename, self.btn_remove]
-        for item in items:
+        for item in [self.btn_add, self.btn_rename, self.btn_remove]:
             toolbar.addWidget(item)
 
         return toolbar
@@ -97,45 +98,28 @@ class ProjectManager(QWidget):
         self.btn_remove.setEnabled(index != -1)
         self.sig_project_changed.emit(index)
 
-    def btn_add_isclicked(self):
-        """Handle when the button to add a new project is clicked."""
-        self.project_cbox.set_edit_mode('add')
-
-    def btn_rename_isclicked(self):
-        """Handle when the button to rename a new project is clicked."""
-        self.project_cbox.set_edit_mode('rename')
-
     def btn_remove_isclicked(self):
         """Handle when the button to delete a project is clicked."""
-        project = self.project_cbox.currentText()
-        index = self.project_cbox.currentIndex()
-
-        msg = ("Are you sure that you want to delete project %s and all "
-               " related frames?<br><br>All data will be lost."
-               ) % project
-        ans = QMessageBox.question(self, 'Delete project', msg,
-                                   defaultButton=QMessageBox.No)
-
-        if ans == QMessageBox.Yes:
-            self.project_cbox.removeItem(index)
-            self.sig_project_removed.emit(project)
+        self.sig_del_project.emit(self.project_cbox.currentText())
 
     # ---- Project combobox handlers
 
-    @property
-    def current_project(self):
+    def currentProject(self):
         """Return the currently selected project."""
         return self.project_cbox.currentText()
 
-    def set_projects(self, projects):
-        """Add a the project list to the project combobox."""
-        self.project_cbox.clear()
-        self.project_cbox.addItems(projects)
+    def currentProjectIndex(self):
+        """Return the index of the currently selected project."""
+        return self.project_cbox.currentIndex()
 
-    def set_current_project(self, project):
+    def setCurrentProject(self, project):
         """Set the current project to project in the combobox."""
         self.project_cbox.setCurentText(project)
-        self.sig_project_changed.emit(self.project_cbox.currentIndex())
+        self.project_changed(self.project_cbox.currentIndex())
+
+    def setCurrentProjectIndex(self, index):
+        """Set the current index of the combobox to the specified index."""
+        self.project_cbox.setCurentIndex(index)
         self.project_changed(self.project_cbox.currentIndex())
 
 
