4c8fb4f02914343d8901ffd13be53bb390b078ad
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-07-24 16:30:41 -0400

    Use a model to show project in the combobox

--- qwatson/managers/projects.py
+++ qwatson/managers/projects.py
@@ -139,8 +139,135 @@ class ProjectManager(QWidget):
         self.project_changed(self.project_cbox.currentIndex())
 
 
+class ProjectView(QWidget):
+    """
+    A QComboxBox underlain with a QLineEdit to display the existing projects
+    in the frame database and to add or rename projects to the database.
+    """
+    currentIndexChanged = QSignal(int)
+    sig_rename_project = QSignal(str, str)
+    sig_add_project = QSignal(str)
+
+    def __init__(self, model, parent=None):
+        super(ProjectView, self).__init__(parent)
+        self._edit_mode = None
+        self.setup()
+        self.combobox.setModel(model)
+
+        # We need to call showPopup on the combobox here to prevent a lag
+        # when clicking on the combobox for the first time.
+        self.combobox.showPopup()
+
+    def setup(self):
+        """Setup the combobox and the lineedit widget."""
+        self.linedit = QLineEdit()
+        self.linedit.setVisible(False)
+        self.linedit.installEventFilter(self)
+
+        self.combobox = QComboBox()
+        self.combobox.setEditable(False)
+        self.combobox.currentIndexChanged.connect(
+            self.currentIndexChanged.emit)
+
+        layout = QGridLayout(self)
+        layout.addWidget(self.combobox, 0, 0)
+        layout.addWidget(self.linedit, 0, 0)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+    def setFixedHeight(self, height):
+        """Extend Qt method setFixedHeight."""
+        self.linedit.setFixedHeight(height)
+        self.combobox.setFixedHeight(height)
+        super(ProjectView, self).setFixedHeight(height)
+
+    def currentText(self):
+        """Return the current text of the combobox."""
+        return self.combobox.currentText()
+
+    def currentIndex(self):
+        """Return the current index of the combobox."""
+        return self.combobox.currentIndex()
+
+    def setCurentText(self, name):
+        """Set the combobox to the index matching the provided name."""
+        self.setCurentIndex(self.combobox.findText(name))
+
+    def setCurentIndex(self, index):
+        """Set the combobox current index the the specified index."""
+        self.combobox.blockSignals(True)
+        self.combobox.setCurrentIndex(index)
+        self.combobox.blockSignals(False)
+
+    def add_project(self):
+        """Show the lineedit to add a project."""
+        self._edit_mode = 'add'
+        self.linedit.clear()
+        self._enter_edit_mode()
+
+    def rename_project(self):
+        """Show the lineedit to rename a project."""
+        self._edit_mode = 'rename'
+        self.linedit.setText(self.combobox.currentText())
+        self._enter_edit_mode()
+
+    def _enter_edit_mode(self):
+        """
+        Enter edit mode either to add a new item or to rename an existing
+        item.
+        """
+        self.linedit.setVisible(True)
+        self.linedit.setFocus(True)
+        self.combobox.setVisible(False)
+
+    def _leave_edit_mode(self):
+        """
+        Leave edit mode by hiding the lineedit and showing back the
+        combobox.
+        """
+        self._edit_mode = None
+        self.linedit.clear()
+        self.combobox.setVisible(True)
+        self.combobox.setFocus()
+        self.linedit.setVisible(False)
+
+    def eventFilter(self, widget, event):
+        """
+        Check for events to accept the edits and modify the content of the
+        combobox accordingly.
+        """
+        is_focusout = event.type() == QEvent.FocusOut
+        is_keypress = event.type() == QEvent.KeyPress
+
+        is_canceled = (
+            is_keypress and event.key() == Qt.Key_Escape
+            ) and self._edit_mode is not None
+        is_accepted = (
+            is_focusout or
+            (is_keypress and event.key() in [Qt.Key_Enter, Qt.Key_Return])
+            ) and self._edit_mode is not None
+
+        new_name = self.linedit.text()
+        if is_accepted and self._edit_mode == 'add':
+            self.sig_add_project.emit(new_name)
+        elif is_accepted and self._edit_mode == 'rename':
+            old_name = self.combobox.currentText()
+            self.sig_rename_project.emit(old_name, new_name)
+
+        if is_accepted or is_canceled:
+            self._leave_edit_mode()
+
+        return QWidget.eventFilter(self, widget, event)
+
+
 if __name__ == '__main__':
+    from qwatson.watson.watsonextends import Watson
+    from qwatson import __rootdir__
+    import os.path as osp
+
+    dirname = osp.join(__rootdir__, 'widgets', 'tests', 'appdir')
+    client = Watson(config_dir=dirname)
+
     app = QApplication(sys.argv)
-    project_manager = ProjectManager()
+    project_manager = ProjectManager(client)
     project_manager.show()
     app.exec_()
